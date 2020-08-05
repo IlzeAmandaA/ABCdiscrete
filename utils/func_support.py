c9e138256cc4be4a919ed599020189b5e547e0b0
IlzeAmandaA <ilze.amanda.auzina@gmail.com>
2020-08-05 17:02:24 +0200

    change ylim

--- utils/func_support.py
+++ utils/func_support.py
@@ -191,7 +191,7 @@ def plot_dist(dict_res, dict_true, location):
            ylabel="Posterior")
 
     plt.xticks(x, [str(id) for id in x])
-    #plt.ylim(-0.02, 0.15)
+    plt.ylim(-25, 15)
     plt.legend()
     plt.savefig(location + '.png')
 
