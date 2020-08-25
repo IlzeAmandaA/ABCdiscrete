225c9d0ff93f2de77842533daa424ffceaf6a245
IlzeAmandaA <ilze.amanda.auzina@gmail.com>
2020-08-25 22:47:24 +0200

    update data loading

--- experiments/mnist.py
+++ experiments/mnist.py
@@ -18,6 +18,10 @@ filename = [
 ["test_labels","t10k-labels-idx1-ubyte.gz"]
 ]
 
+def transform_polar(image):
+    image[image<0.5]=-1
+    image[image>=0.5]=1
+    # return image
 
 class MNIST():
     def __init__(self, l1=0, l2=1, H=20, name='mnist', image_size=(14, 14), batch_size=1000, train_size=5000):
@@ -36,34 +40,42 @@ class MNIST():
         x_test = x_test / 255.
 
         # TRAIN DATA
-        idx_l1 = np.where(y_train==l1)[0]
-        idx_l2 = np.where(y_train==l2)[0]
-        idx = np.sort(np.concatenate((idx_l1[0:int(train_size/2)], idx_l2[0:int(train_size/2)]), axis=None))
+        idx_l1 = np.where(y_train == l1)[0]
+        idx_l2 = np.where(y_train == l2)[0]
+        idx = np.sort(np.concatenate((idx_l1, idx_l2), axis=None))
+        train_size = len(idx)
+        # print(len(idx1))
+        # idx = np.sort(np.concatenate((idx_l1[0:int(train_size / 2)], idx_l2[0:int(train_size / 2)]), axis=None))
 
-
-        x_train= np.reshape(x_train[idx], (train_size, 28, 28))
+        x_train = np.reshape(x_train[idx], (train_size, 28, 28))
         self.x_train = np.zeros((x_train.shape[0], image_size[0], image_size[1]))
         for i in range(x_train.shape[0]):
             self.x_train[i] = resize(x_train[i], image_size, anti_aliasing=True)
         self.x_train = np.reshape(self.x_train, (train_size, image_size[0] * image_size[1]))
-        self.y_train = y_train[idx] #[0:train_size]
+        self.y_train = y_train[idx]  # [0:train_size]
         assert self.x_train.shape[0] == self.y_train.shape[0], 'incorrect dim xtrain and ytrain'
+        # self.x_train = transform_polar(self.x_train)
+        transform_polar(self.x_train)
+
+        print('Shape of train data {}'.format(self.x_train.shape))
+
 
         # TEST DATA
-        idx_l1 = np.where(y_test==l1)[0]
-        idx_l2 = np.where(y_test==l2)[0]
+        idx_l1 = np.where(y_test == l1)[0]
+        idx_l2 = np.where(y_test == l2)[0]
         idx = np.sort(np.concatenate((idx_l1, idx_l2), axis=None))
 
         x_test = np.reshape(x_test[idx], (len(idx), 28, 28))
-        self.x_test = np.zeros((len(idx), image_size[0], image_size[1]))
-        for i in range(len(idx)):
+        self.x_test = np.zeros((x_test.shape[0], image_size[0], image_size[1]))
+        for i in range(x_test.shape[0]):
             self.x_test[i] = resize(x_test[i], image_size, anti_aliasing=True)
-        self.x_test = np.reshape(self.x_test, (len(idx), image_size[0] * image_size[1]))
+        self.x_test = np.reshape(self.x_test, (self.x_test.shape[0], image_size[0] * image_size[1]))
         self.y_test = y_test[idx]
-        assert self.x_test.shape[0] == self.y_test.shape[0], 'incorrect dim xtrain and ytrain'
+        assert self.x_test.shape[0] == self.y_test.shape[0], 'incorrect dim xtest and ytest'
+        # self.x_test = transform_polar(self.x_test)
+        transform_polar(self.x_test)
 
-        self.batch_size = batch_size
-        self.transform()
+        print('Shape of test data {}'.format(self.x_test.shape))
 
 
 
@@ -100,35 +112,35 @@ class MNIST():
         self.x_test[self.x_test < 0.5] = -1
         self.x_test[self.x_test > 0.5] = 1
 
-
-    def visualize(self, mode='train', size_x=5, size_y=5, file_name='mnist_images'):
-        fig = plt.figure(figsize=(size_x, size_y))
-        gs = gridspec.GridSpec(size_x, size_y)
-        gs.update(wspace=0.05, hspace=0.05)
-
-        if mode == 'train':
-            x_sample = self.x_train[0:size_x * size_y]
-        elif mode == 'test':
-            x_sample = self.x_test[0:size_x * size_y]
-        else:
-            raise ValueError('Mode could be train OR test, nothing else!')
-
-        for i, sample in enumerate(x_sample):
-            ax = plt.subplot(gs[i])
-            plt.axis('off')
-            ax.set_xticklabels([])
-            ax.set_yticklabels([])
-            ax.set_aspect('equal')
-            sample = np.expand_dims(sample, 0)
-            sample = sample.reshape((1, self.image_size[0], self.image_size[1]))
-            sample = sample.swapaxes(0, 2)
-            sample = sample.swapaxes(0, 1)
-            sample = sample[:, :, 0]
-            plt.imshow(sample, cmap='gray')
-
-        plt.savefig(file_name + '_' + mode + '.pdf', bbox_inches='tight')
-        plt.close(fig)
-
+    #
+    # def visualize(self, mode='train', size_x=5, size_y=5, file_name='mnist_images'):
+    #     fig = plt.figure(figsize=(size_x, size_y))
+    #     gs = gridspec.GridSpec(size_x, size_y)
+    #     gs.update(wspace=0.05, hspace=0.05)
+    #
+    #     if mode == 'train':
+    #         x_sample = self.x_train[0:size_x * size_y]
+    #     elif mode == 'test':
+    #         x_sample = self.x_test[0:size_x * size_y]
+    #     else:
+    #         raise ValueError('Mode could be train OR test, nothing else!')
+    #
+    #     for i, sample in enumerate(x_sample):
+    #         ax = plt.subplot(gs[i])
+    #         plt.axis('off')
+    #         ax.set_xticklabels([])
+    #         ax.set_yticklabels([])
+    #         ax.set_aspect('equal')
+    #         sample = np.expand_dims(sample, 0)
+    #         sample = sample.reshape((1, self.image_size[0], self.image_size[1]))
+    #         sample = sample.swapaxes(0, 2)
+    #         sample = sample.swapaxes(0, 1)
+    #         sample = sample[:, :, 0]
+    #         plt.imshow(sample, cmap='gray')
+    #
+    #     plt.savefig(file_name + '_' + mode + '.pdf', bbox_inches='tight')
+    #     plt.close(fig)
+    #
 
     def bern2(self, p, D1, D2):
         return 2. * np.random.binomial(1, p, (D1, D2)) - 1.
