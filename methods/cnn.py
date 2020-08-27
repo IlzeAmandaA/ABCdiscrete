4a9ba339b986e727467eb9329da7deeedcfc1e0d
IlzeAmandaA <ilze.amanda.auzina@gmail.com>
2020-08-27 23:28:12 +0200

    er

--- methods/cnn.py
+++ methods/cnn.py
@@ -1,9 +1,7 @@
 import torch
 import torch.nn as nn
 import torch.utils.data
-from torch.autograd import Function
-import torch.nn.functional as F
-import numpy as np
+
 
 
 class Network_CNN(nn.Module):
@@ -22,16 +20,6 @@ class Network_CNN(nn.Module):
         self.conv2 = nn.Conv2d(self.K1,self.K2, kernel_size=self.F, padding=2, bias=False)
         self.fc = nn.Linear(4*4*1, self.outD, bias=False)
         self.soft = nn.Softmax(dim=0)
-        # self.layer1 = nn.Sequential(
-        #     nn.Conv2d(in_features, num_units, kernel_size=3, padding=1, bias=False),
-        #     nn.MaxPool2d(2))
-        # self.layer2 = nn.Sequential(
-        #     nn.Conv2d(num_units,1, kernel_size=3, padding=2, bias=False),
-        #     nn.MaxPool2d(2))
-        # self.fc=nn.Sequential(
-        #     nn.Linear(4*4*1, out_features, bias=False),
-        #     nn.Softmax(dim=0))
-
 
     def forward(self, x):
         out = self.conv1(x)
@@ -46,6 +34,7 @@ class Network_CNN(nn.Module):
 
 
     def calculate_classification_error(self, X,Y):
+        print('calculating error')
         Y_hat = self.forward(X)
         error = 1. - Y_hat.eq(Y).cpu().float().mean().item()
         return error, Y_hat
