2594f40f843d6e385f396e58cc7237380814d769
IlzeAmandaA <ilze.amanda.auzina@gmail.com>
2020-08-29 21:34:21 +0200

    rm check

--- experiments/mnist_torch.py
+++ experiments/mnist_torch.py
@@ -143,9 +143,6 @@ class HighDim():
 
     def __init__(self, inD=1, outD=10, rescale=14):
 
-        print('cuda count', torch.cuda.device_count())
-        print('cuda name', torch.cuda.get_device_name(0))
-        print(' avail', torch.cuda.is_available())
 
         self.cuda_available = torch.cuda.is_available()
 
